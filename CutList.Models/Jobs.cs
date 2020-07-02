f08e032912a36d698835008b506921824511640a
Paul Lynch <41754007+PaulLynch123@users.noreply.github.com>
2020-07-02 12:55:34 +0100

    starting to make models of Database tables

---
+++ CutList.Models/Jobs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CutList.Models
+{
+    class Jobs
+    {
+        [Key]
+        public int Won { get; set; }
+
+        public string Prod_Range { get; set; }      //enum
+        public DateTime Rqd_Ship_Date { get; set; }
+        public DateTime Orig_Rqd_Ship_Date { get; set; }
+        public DateTime P_Manu_F { get; set; }
+        [Display(Name ="Approval Status")]
+        public bool Status { get; set; }
+
+        public string Project_Id { get; set; }      //from another database
+        public string Project_Name { get; set; }    //from another database
+        public int BB_Run { get; set; }
+        public int Batch_Details { get; set; }
+        public string Job_Notes { get; set; }
+        public string Engineer { get; set; }        //list of employee names populated from where?
+        public string Checked_By { get; set; }      //list of approvers names
+        public string Joint_Pack_Check { get; set; }    //mostly null not sure what for
+        public string Ip { get; set; }                  //usually IP55... not sure what this is?
+        public bool Heat_Sink { get; set; }             //yes or No
+        public DateTime Date_Modified { get; set; }     //is this a typed date or can I get it from system? Does it record when required date changed
+        public string Label { get; set; }               //is there a list here too or is it free text?
+        public bool Tinner_Option { get; set; }         //yes or no for Tinned or not. Will this ever change?
+        public bool Date_Changed_Flag { get; set; }     //notification that there is a date change
+        public bool Specials_Included { get; set; }     //yes or no for notification of specials
+        public bool Label_Ka { get; set; }              //what is KA???
+        public bool Label_Neutral { get; set; }         //yes no if Neutral
+        public bool Label_Earth { get; set; }           //yes no if Earth
+
+
+        //--------- navigation links in Tables -----------
+        
+
+    }
+}
